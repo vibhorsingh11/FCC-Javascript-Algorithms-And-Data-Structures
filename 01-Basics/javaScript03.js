71cfad6bac04ae5d8bfb30d4c4465c0ce24b9a78
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-04-22 15:21:47 +0530

    Adding Challenge 03 to JavaScript

---
+++ 01-Basics/javaScript03.js
@@ -0,0 +1,17 @@
+/*In JavaScript, you can store a value in a variable with the assignment operator.
+
+myVariable = 5;
+
+This assigns the Number value 5 to myVariable.
+
+Assignment always goes from right to left. Everything to the right of the = operator is resolved before the value is assigned to the variable to the left of the operator.
+
+myVar = 5;
+myNum = myVar;
+This assigns 5 to myVar and then resolves myVar to 5 again and assigns it to myNum.
+
+
+Assign the value 7 to variable a.
+
+Assign the contents of a to variable b.*/
+
