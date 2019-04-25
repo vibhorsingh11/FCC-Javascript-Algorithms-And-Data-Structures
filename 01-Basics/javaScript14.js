7785553b9361f2a9259d13a57956534937dcc9e5
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-04-25 11:56:16 +0530

    Adding Challenge 14 to Basics

---
+++ 01-Basics/javaScript14.js
@@ -0,0 +1,11 @@
+/**
+ * In JavaScript, you can also perform calculations with decimal numbers, just like whole numbers.
+
+Let's multiply two decimals together to get their product.
+
+
+Change the 0.0 so that product will equal 5.0.
+ */
+
+var product = 5.0 * 1.0;
+
