6d4c34cef23cf5935766339f44366b611e954fa1
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-04-27 12:46:58 +0530

    Adding Challenge 28 to Basics

---
+++ 01-Basics/javaScript28.js
@@ -0,0 +1,17 @@
+/**
+ * Just as we can build a string over multiple lines out of string literals, we can also append variables to a string using the plus equals (+=) operator.
+
+
+Set someAdjective and append it to myStr using the += operator.
+ */
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "freeCodeCamp";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
