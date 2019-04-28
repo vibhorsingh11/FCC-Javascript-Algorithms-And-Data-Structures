30ee7c458fd1af7cdd43c24d205681529db88109
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-04-28 12:37:22 +0530

    Adding Challenge 36 to Basics

---
+++ 01-Basics/javaScript36.js
@@ -0,0 +1,19 @@
+/**
+ * With JavaScript array variables, we can store several pieces of data in one place.
+
+You start an array declaration with an opening square bracket, end it with a closing square bracket, and put a comma between each entry, like this:
+
+var sandwich = ["peanut butter", "jelly", "bread"].
+
+
+Modify the new array myArray so that it contains both a string and a number (in that order).
+
+Hint
+Refer to the example code in the text editor if you get stuck.
+ */
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["Vibhor", 25];
