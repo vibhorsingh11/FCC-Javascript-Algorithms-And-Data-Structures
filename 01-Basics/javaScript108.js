6240837a8eba75900628728edf0eb9a41c3ee982
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-05-06 14:37:28 +0530

    Adding Challenge 108 to Basics

---
+++ 01-Basics/javaScript108.js
@@ -0,0 +1,30 @@
+/**
+ * In the previous challenge, you used a single conditional operator. You can also chain them together to check for multiple conditions.
+
+The following function uses if, else if, and else statements to check multiple conditions:
+
+function findGreaterOrEqual(a, b) {
+  if(a === b) {
+    return "a and b are equal";
+  }
+  else if(a > b) {
+    return "a is greater";
+  }
+  else {
+    return "b is greater";
+  }
+}
+The above function can be re-written using multiple conditional operators:
+
+function findGreaterOrEqual(a, b) {
+  return (a === b) ? "a and b are equal" : (a > b) ? "a is greater" : "b is greater";
+}
+
+Use multiple conditional operators in the checkSign function to check if a number is positive, negative or zero.
+ */
+
+function checkSign(num) {
+    return num === 0 ? "zero" : num > 0 ? "positive" : "negative";  
+  }
+  
+  checkSign(10);
