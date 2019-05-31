f50140a98dde770a89915c99c00a1fd103cd541b
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-05-31 15:45:06 +0530

    Adding Challenge 16 to BasicAlgo

---
+++ 06-BasicAlgo/algo16.js
@@ -0,0 +1,25 @@
+/**
+ * Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+ */
+
+function chunkArrayInGroups(arr, size) {
+
+    var temp = [];
+    var result = [];
+
+    for (var a = 0; a < arr.length; a++) {
+      if (a % size !== size - 1)
+        temp.push(arr[a]);
+      else {
+        temp.push(arr[a]);
+        result.push(temp);
+        temp = [];
+      }
+    }
+
+    if (temp.length !== 0)
+      result.push(temp);
+    return result;
+  }
