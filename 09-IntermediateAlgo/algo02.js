64b27a3eebd1f83ecd2050adc0295884e95ae4f3
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-06-16 21:29:14 +0530

    Adding Challenge 02 to IntermediateAlgo

---
+++ 09-IntermediateAlgo/algo02.js
@@ -0,0 +1,29 @@
+/**
+ * Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Note
+You can return the array with its elements in any order.
+ */
+
+function diffArray(arr1, arr2) {
+    var newArr = [];
+
+    function onlyInFirst(first, second) {
+    // Looping through an array to find elements that don't exist in another array
+      for (var i=0;i<first.length;i++) {
+        if (second.indexOf(first[i]) === -1) {
+          // Pushing the elements unique to first to newArr
+          newArr.push(first[i]);
+        }
+      }
+    }
+
+    onlyInFirst(arr1, arr2);
+    onlyInFirst(arr2, arr1);
+
+    return newArr;
+  }
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
