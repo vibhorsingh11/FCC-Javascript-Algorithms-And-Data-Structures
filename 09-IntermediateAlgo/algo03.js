a18e2fabc449203f87cc20e7e6344d594e88abd7
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-06-17 11:24:03 +0530

    Adding Challenge 03 to IntermediateAlgo

---
+++ 09-IntermediateAlgo/algo03.js
@@ -0,0 +1,24 @@
+/**
+ * You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+
+Note
+You have to use the arguments object.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+ */
+
+function destroyer(arr) {
+    // Remove all the values
+    var args = Array.prototype.slice.call(arguments);
+  
+    for (var i = 0; i < arr.length; i++) {
+      for (var j = 0; j < args.length; j++) {
+        if (arr[i] === args[j]) {
+          delete arr[i];
+        }
+      }
+    }
+    return arr.filter(Boolean);
+  }
+  
+  destroyer([1, 2, 3, 1, 2, 3], 2, 3);
