a47d06e43531944a565f7e1f97817c4631782821
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-06-20 13:09:10 +0530

    Adding Challenge 18 to IntermediateAlgo

---
+++ 09-IntermediateAlgo/algo18.js
@@ -0,0 +1,24 @@
+/**
+ * Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
+In other words, you are given an array collection of objects. The predicate pre will be an object property and you need to return true if its value is truthy. Otherwise, return false.
+
+In JavaScript, truthy values are values that translate to true when evaluated in a Boolean context.
+
+Remember, you can access object properties through either dot notation or [] notation.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+ */
+
+function truthCheck(collection, pre) {
+    // Is everyone being true?
+    var counter = 0;
+    for (var c in collection) {
+      if (collection[c].hasOwnProperty(pre) && Boolean(collection[c][pre])) {
+        counter++;
+      }
+    }
+    return counter == collection.length;
+  }
+  
+  truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
