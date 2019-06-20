6473d07482ed0c3e4882dd9ab32a161a64a300b7
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-06-20 12:29:33 +0530

    Adding Challenge 09 to IntermediateAlgo

---
+++ 09-IntermediateAlgo/algo09.js
@@ -0,0 +1,19 @@
+/**
+ * Find the missing letter in the passed letter range and return it.
+
+If all letters are present in the range, return undefined.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+ */
+
+function fearNotLetter(str) {
+    for(var i = 0; i < str.length; i++) {
+      var code = str.charCodeAt(i);
+      if (code !== str.charCodeAt(0) + i) {
+        return String.fromCharCode(code - 1);
+      }  
+    }
+    return undefined;
+  }
+  
+  fearNotLetter("abce");
