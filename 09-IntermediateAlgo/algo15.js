563f37fe0c644487000f34bb7aa5e4e5724cdb0d
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-06-20 12:40:40 +0530

    Adding Challenge 15 to IntermediateAlgo

---
+++ 09-IntermediateAlgo/algo15.js
@@ -0,0 +1,22 @@
+/**
+ * Given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
+
+Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+ */
+
+function dropElements(arr, func) {
+    // Drop them elements.
+    var times = arr.length;
+    for (var i = 0; i < times; i++) {
+      if (func(arr[0])) {
+        break;
+      } else {
+        arr.shift();
+      }
+    }
+    return arr;
+  }
+  
+  dropElements([1, 2, 3], function(n) {return n < 3; });
