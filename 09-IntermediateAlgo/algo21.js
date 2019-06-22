6251aee1747628853db448f34d9b0305614b223c
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-06-22 13:00:06 +0530

    Adding Challenge 21 to IntermediateAlgo

---
+++ 09-IntermediateAlgo/algo21.js
@@ -0,0 +1,36 @@
+/**
+ * Return a new array that transforms the elements' average altitude into their orbital periods (in seconds).
+
+The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+
+You can read about orbital periods on Wikipedia.
+
+The values should be rounded to the nearest whole number. The body being orbited is Earth.
+
+The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+ */
+
+function orbitalPeriod(arr) {
+    var GM = 398600.4418;
+    var earthRadius = 6367.4447;
+    var a = 2 * Math.PI;
+    var newArr = [];
+    var getOrbPeriod = function(obj) {
+      var c = Math.pow(earthRadius + obj.avgAlt, 3);
+      var b = Math.sqrt(c / GM);
+      var orbPeriod = Math.round(a * b);
+      delete obj.avgAlt;
+      obj.orbitalPeriod = orbPeriod;
+      return obj;
+    };
+  
+    for (var elem in arr) {
+      newArr.push(getOrbPeriod(arr[elem]));
+    }
+  
+    return newArr;
+  }
+  
+  orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
